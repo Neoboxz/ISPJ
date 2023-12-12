96a804594a9973a6b416d538eb1788a3693d7007
Neoboxz <neoplayz22@gmail.com>
2023-12-12 23:17:26 +0800

    e

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -1,5 +1,20 @@
-export default function home(){
+import { data } from "autoprefixer"
+import React, { useEffect , useState } from "react"
+
+
+export default async function home(){
+    const [message , setMessage] = useState("loading")
+    useEffect(()=>{
+        fetch("http://localhost:5000/api/home").then(
+            response => response.json
+        ).then(
+            data => {
+                console.log(data)
+                setMessage(data.message)
+            }
+        )
+    }, [] )
     return(
-        <div>test</div>
+        <div></div>
     )
 }
