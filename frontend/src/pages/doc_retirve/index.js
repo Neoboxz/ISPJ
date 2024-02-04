cce62c700de4b0f299f75c26db73d032ecb68260
Neoboxz <neoplayz22@gmail.com>
2024-02-04 22:38:34 +0800

    Revert "retrieving of data start"
    This reverts commit d152f0113cac45fb56a8f8623d452ee31166f1fa.

--- frontend/src/pages/doc_retirve/index.js
+++ frontend/src/pages/doc_retirve/index.js
@@ -1,30 +1,23 @@
-import { useEffect } from "react"
-
+import { firestoreRetrieveDocumentField } from '@/firebase/firestore'
+import { useEffect } from 'react'
 
 export default function doc_retrive() {
-    useEffect(()=>{
-        asdf()
-    })
-
-    const asdf = async () => {
-        console.log('fetching api')
-        const yes = await fetch('http://localhost:5001/api/2', {
-        method: 'GET'
-      })
-      console.log(yes)
-    }
-  
-    return (
-      <>
-      testing
-      <button
-      onClick={() => {
-        
-      }}
-      >
-        
-      </button>
-      </>
-      
+  const asdf = async () => {
+    const health_document = await firestoreRetrieveDocumentField(
+      'patient/111',
+      'health_document',
     )
+    console.log(health_document)
   }
+
+  useEffect(() => {
+    asdf()
+  })
+
+  return (
+    <>
+      testing
+      <button onClick={() => {}}>asdf</button>
+    </>
+  )
+}
